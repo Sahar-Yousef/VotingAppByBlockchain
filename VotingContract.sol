e90d9bcf52fbd5c4a7e1ea75b84d1b3f188cf284
Mashael <108432621+Mashael-M@users.noreply.github.com>
2022-07-29 16:51:13 +0300

    Update VotingContract.sol

--- VotingContract.sol
+++ VotingContract.sol
@@ -1,2 +1,25 @@
-//hi
+// SPDX-License-Identifier: GPL-3.0
 
+pragma solidity >=0.7.0 <0.9.0;
+
+contract VotingContract {
+    int private Yescount = 0;
+    int private Nocount = 0;
+
+    //  Yes counter
+    function incYesCounter() public {
+        Yescount += 1;
+    }
+    //  No counter
+     function incNoCounter() public {
+        Nocount += 1;
+    }
+    //  Yes Total counter
+    function getTotalYes() public view returns (int) {
+        return Yescount;
+    }
+    //  No Total counter
+    function getTotalNo() public view returns (int) {
+        return Nocount;
+    }
+}
